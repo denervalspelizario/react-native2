d6e4d543c2bcd12daf7e809d08ab3d78bf2d74fd
denervalspelizario <denervalspelizario@gmail.com>
2023-02-20 12:22:37 -0300

    estudo react-native uso de useRef

--- 23_UseRef/App.js
+++ 23_UseRef/App.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect, useMemo} from 'react';  // 1 importando useMemo  depois linha
+import React, {useState, useEffect, useMemo, useRef} from 'react'; // 1 importando o useRef
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';  
-import AsyncStorage  from '@react-native-async-storage/async-storage';  // importando o asyncStorage
+import AsyncStorage  from '@react-native-async-storage/async-storage';  
 
 // o UseEffect é um componente que será chamado toda vez que for inicializado o app ou atualizado 
 // leia linhas 20 e 38 o useEffect ele basicamente entra no lugar de Component didMount e Component didUpdate
@@ -16,6 +16,7 @@ export default function App(){
      ** o valor dentro de parentes do useState é o valor inicial da state no caso  nome = ''    */       
   const [nome, setNome] = useState('')  // state nome inicia vazia
   const [input, setInput] = useState('') // state input inicia vazia
+  const nomeInput  = useRef(null) // 1 o valor null indica que o useRef inicial nulo, o nomeInput pode ser qualuqer nome  
 
   /*ComponentDidMount - Este método é executado uma vez em um ciclo de vida de um componente e será após a primeira renderização. 
     o useEffect inicia  com uma funcao(()={}) e depois um [] dentro desse [] fica o dado que vai ser alterado ou seja a funcao só sera chamada usada
@@ -23,10 +24,10 @@ export default function App(){
   useEffect(()=> {
     
     async function pegarStorage(){
-      const nomeStorage = await AsyncStorage.getItem('nomes');
-
-      if(nomeStorage !== null){
-        setNome(nomeStorage)
+      const nomeStorage = await AsyncStorage.getItem('nomes'); // funcao é chamda quando se incia e pega oque tiver em nomes(leia linha 44) e adiciona em setNome ou seja
+                                                               // nome se inicia vazio ou '' mas essa funcao joga o 'nomes' no setNome(altera state) tranformando
+      if(nomeStorage !== null){                                // nome vazio em conteudo que estiver em 'nomes'  no caso o ultimo nome digitado funciona como um save  
+        setNome(nomeStorage)                                   // leia linha 41 e entanda o a funcao salvarStorage 
       }
 
     }
@@ -41,10 +42,10 @@ export default function App(){
   useEffect(()=>{   // toda vez que nome(linha 11) for alterado ele executa a funcao saveStorage, que guarda o nome
 
     async function salvarStorage(){
-      await AsyncStorage.setItem('nomes', nome);
-    }
-
-    salvarStorage();
+      await AsyncStorage.setItem('nomes', nome);  // funcao pega oque tiver em nome(linha 17) e salva lembre-se ela só é chamada quando nome for alterado
+    }                                             // no caso quando for digitado algo no input   
+                                                  // ou seja vai SALVAR o conteudo de nome em 'nomes' depois que nome queceber dado da input
+    salvarStorage();                              // que posteriormente será usado leia linha 23
 
   }, [nome])  // quando meu NOME for alterado
 
@@ -53,6 +54,10 @@ export default function App(){
     setNome(input);
     setInput('');                
   }
+
+  function novoNome(){ // 1 funcao que altera a qualquer nome
+    nomeInput.current.focus(); // 1 como foi referenciao o nomeInput ao input(linha 79) ao clicar botao(linha 90) chama funcao novoNome que foca(ativa o elemento)  input 
+  }                            // 1 ao ativar o elemento input, abre o teclado deixando pronto paa digitar algo  
 
   const letrasNome = useMemo(() => { /*console.log('mudou letra');*/  return nome.length} ,[nome] )   // 1 primeiro parametro é uma funcao  e o colchete é um array de dependecia
   /* a funcao useMemo precisa ser importada, e tem como funcao diminuir uso de memoria ao se usa-la neste exeplo ele somente vai ser renderizado apos o clique
@@ -66,12 +71,12 @@ export default function App(){
         value={input}
         style={styles.input}  // estilizacao
         placeholder=' Digite seu nome completo' // adiciona uma string temporaria até digitar algum texto
-        underlineColorAndroid="transparent"  // 1 retira o Underline padrao do texto digitado
-        onChangeText={(texto) => setInput(texto)} // 1 onChangeText pega o texto digitado no input 
-                                                            // 2 com essa funcao ao digitar o state que é "Desenvovledor" 
-                                                            // 2 que muda para setState que é o texto recebido pelo parametro texto 
+        underlineColorAndroid="transparent"  //  retira o Underline padrao do texto digitado
+        onChangeText={(texto) => setInput(texto)} //  onChangeText pega o texto digitado no input 
+                                                            //  com essa funcao ao digitar o state que é "Desenvovledor" 
+                                                            //  que muda para setState que é o texto recebido pelo parametro texto 
         maxLength={40} //maximo de caracteres      
-      
+        ref={nomeInput} // 1 referencia o input a const nomeInput linha 19
       />
       
                                            {/* Ao clicar chama a funcao alteraNome*/}
@@ -82,6 +87,11 @@ export default function App(){
       </TouchableOpacity>
       <Text style={styles.nome}>{nome}</Text>
       <Text style={styles.nome}>Tem {letrasNome} letras</Text>
+      <TouchableOpacity style={styles.btn} onPress={novoNome} > 
+        <Text style={{color:'#FFF'}}>
+          Novo Nome
+        </Text>
+      </TouchableOpacity>
     </View>
 
   )
